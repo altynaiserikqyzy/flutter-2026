a4cca2bf3f3a8effb67e88383dcaf4408b5c8fe5
Алтынай <altynaiserikqyzyy@gmail.com>
2026-09-16 14:31:56 +0500

    week02

---
+++ lib/week02/main.dart
@@ -0,0 +1,35 @@
+import 'data.dart';
+import 'models.dart';
+import 'catalogue.dart';
+import 'shelf_state.dart';
+
+void main() {
+  final books = rawBooks
+      .map((json) => Book.fromJson(json))
+      .toList();
+
+  final library = Library();
+
+  for (final book in books) {
+    library.addItem(book);
+  }
+
+  library.open();
+
+  print('Titles: ${library.titles}');
+  print('Books after 2010: ${library.booksAfter2010}');
+  print('Average pages: ${library.averagePages}');
+  print('Books by author: ${library.booksByAuthor}');
+  print('Author names: ${library.authorNames}');
+  print('Genres: ${library.genres}');
+  print('Display list: ${library.displayList}');
+
+  final stats = statsOf(books);
+
+  print('Count: ${stats.count}');
+  print('Average pages from record: ${stats.avgPages}');
+
+  print(describe(Empty()));
+  print(describe(Ready(books)));
+  print(describe(Broken('Shelf needs repair')));
+}

455f232bd3510c1dfcc50f117e44ec1cc33b0c6a
Алтынай <altynaiserikqyzyy@gmail.com>
2026-09-23 22:19:14 +0500

    chore: clean repository structure

--- week_3/lib/data.dart
+++
@@ -1,9 +0,0 @@
-const String myName = 'Altynai Rgalbekova';
-const String myUniversity = 'Kazakh-British Technical University';
-
-const List<({String label, String value})> facts = [
-  (label: 'Course', value: '2'),
-  (label: 'Group', value: '24BD'),
-  (label: 'Subject', value: 'Flutter I'),
-  (label: 'Favourite widget', value: 'Padding'),
-];
